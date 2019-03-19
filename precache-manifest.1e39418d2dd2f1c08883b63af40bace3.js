c3b874375b96730b5aab52706e193c2d29324b32
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2019-03-19 18:18:07 +0500

    20190319 Build of portfolio

---
+++ precache-manifest.1e39418d2dd2f1c08883b63af40bace3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "504356505aaad25db184",
+    "url": "/static/css/main.2df0c734.chunk.css"
+  },
+  {
+    "revision": "504356505aaad25db184",
+    "url": "/static/js/main.2002a526.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "786d33ed0f934644f508",
+    "url": "/static/css/2.c00c332e.chunk.css"
+  },
+  {
+    "revision": "786d33ed0f934644f508",
+    "url": "/static/js/2.7c3d9a00.chunk.js"
+  },
+  {
+    "revision": "f53c64601c664f3ef04d94ff288d2931",
+    "url": "/static/media/gradient-generator.f53c6460.png"
+  },
+  {
+    "revision": "0f4d0084dc4f7657471a1def52909d60",
+    "url": "/index.html"
+  }
+];
